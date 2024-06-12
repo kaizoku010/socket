ff8d37ef80cfab9cb68d1883218bb6a9dec45df0
mRodneyMo <motokads@gmail.com>
2024-06-12 16:22:00 +0300

    socket.io

---
+++ .history/server_20240612161646.js
@@ -0,0 +1,24 @@
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  
+  // Handle incoming messages
+  ws.on('message', (message) => {
+    console.log('Received message From React Native:', message.toString());
+    // Broadcast the received message to all clients
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  // Handle disconnections
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
+
+console.log('WebSocket server listening on port 8080');
