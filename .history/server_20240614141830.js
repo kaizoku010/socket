a747b80992d0c00ba18f7e3c0d7fd58bf5ab2894
mRodneyMo <motokads@gmail.com>
2024-06-14 14:18:50 +0300

    updated cors 2

---
+++ .history/server_20240614141830.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: '*', // Allow all origins
+    methods: ['GET', 'POST']
+  }
+});
+
+app.use(cors());
+
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  
+    socket.on('disconnect', () => {
+      console.log('Client disconnected');
+    });
+  
+  });
+});
+
+server.listen(8080, () => {
+  console.log('Listening on port 8080');
+});
